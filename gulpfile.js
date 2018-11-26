f146d096c30b485ec16d0a524f1fd161635d6537
Dariusz Luksza <dariusz@luksza.org>
2018-11-26 00:09:30 +0000

    Output vendor files to prod directory

--- gulpfile.js
+++ gulpfile.js
@@ -29,37 +29,37 @@ gulp.task('vendor', function() {
       '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
       '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
     ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
+    .pipe(gulp.dest('./prod/vendor/bootstrap'))
 
   // Font Awesome 5
   gulp.src([
       './node_modules/@fortawesome/**/*'
     ])
-    .pipe(gulp.dest('./vendor'))
+    .pipe(gulp.dest('./prod/vendor'))
 
   // jQuery
   gulp.src([
       './node_modules/jquery/dist/*',
       '!./node_modules/jquery/dist/core.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery'))
+    .pipe(gulp.dest('./prod/vendor/jquery'))
 
   // jQuery Easing
   gulp.src([
       './node_modules/jquery.easing/*.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
+    .pipe(gulp.dest('./prod/vendor/jquery-easing'))
 
   // Simple Line Icons
   gulp.src([
       './node_modules/simple-line-icons/fonts/**',
     ])
-    .pipe(gulp.dest('./vendor/simple-line-icons/fonts'))
+    .pipe(gulp.dest('./prod/vendor/simple-line-icons/fonts'))
 
   gulp.src([
       './node_modules/simple-line-icons/css/**',
     ])
-    .pipe(gulp.dest('./vendor/simple-line-icons/css'))
+    .pipe(gulp.dest('./prod/vendor/simple-line-icons/css'))
 
 });
 
